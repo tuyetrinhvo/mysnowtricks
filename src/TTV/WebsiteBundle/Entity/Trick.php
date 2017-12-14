363e223873a59e69f5c278cc59ab269227987abb
tuyetrinhvo <you@example.com>
2017-12-14 17:15:40 +0100

    Add attributes updatedAt and slug to trick.php

--- src/TTV/WebsiteBundle/Entity/Trick.php
+++ src/TTV/WebsiteBundle/Entity/Trick.php
@@ -3,12 +3,14 @@
 namespace TTV\WebsiteBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * Trick
  *
  * @ORM\Table(name="trick")
  * @ORM\Entity(repositoryClass="TTV\WebsiteBundle\Repository\TrickRepository")
+ * @ORM\HasLifecycleCallbacks
  */
 class Trick
 {
@@ -43,6 +45,17 @@ class Trick
     private $description;
 
     /**
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @Gedmo\Slug(fields={"name"})
+     * @ORM\Column(name="slug", type="string", length=255, unique=true)
+     */
+    private $slug;
+
+    /**
      * @ORM\OneToOne(targetEntity="TTV\WebsiteBundle\Entity\Image", cascade={"persist"})
      */
     private $image;
@@ -69,6 +82,13 @@ class Trick
         $this->date = new \DateTime();
     }
 
+    /**
+     * @ORM\PreUpdate
+     */
+    public function updateDate()
+    {
+        $this->setUpdatedAt(new \DateTime());
+    }
     /**
      * Get id
      *
@@ -245,5 +265,53 @@ class Trick
     public function getUser()
     {
         return $this->user;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Trick
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Trick
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
     }
 }
