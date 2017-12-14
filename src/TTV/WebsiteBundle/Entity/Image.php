1d68f305af9bb8df4ef79326588c6a55cd642882
tuyetrinhvo <you@example.com>
2017-12-14 11:59:28 +0100

    mapping the relationship of entities

--- src/TTV/WebsiteBundle/Entity/Image.php
+++ src/TTV/WebsiteBundle/Entity/Image.php
@@ -3,12 +3,15 @@
 namespace TTV\WebsiteBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Image
  *
  * @ORM\Table(name="image")
  * @ORM\Entity(repositoryClass="TTV\WebsiteBundle\Repository\ImageRepository")
+ * @ORM\HasLifecycleCallbacks
  */
 class Image
 {
@@ -35,6 +38,13 @@ class Image
      */
     private $alt;
 
+    /**
+     * @var UploadedFile
+     * @Assert\Image
+     */
+    private $file;
+
+    private $tempFilename;
 
     /**
      * Get id
@@ -93,5 +103,94 @@ class Image
     {
         return $this->alt;
     }
-}
 
+    /**
+     * Get File
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * Set File
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file)
+    {
+        $this->file = $file;
+        if(null !== $this ->extension){
+            $this->tempFilename = $this->extension;
+
+            $this->extension = null;
+            $this->alt = null;
+
+        }
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        if (null === $this ->file){
+            return;
+        }
+
+        $this->extension = $this->file->guessExtension();
+        $this->alt = $this->file->getClientOriginalName();
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        if(null === $this->file){
+            return;
+        }
+
+        if(null !== $this->tempFilename){
+            $oldFile = $this->getUploadRootDir().'/'.$this->id.'.'.$this->tempFilename;
+            if(file_exists($oldFile)){
+                unlink($oldFile);
+            }
+        }
+        $this->file->move($this->getUploadRootDir(), $this->id.'.'.$this->extension);
+    }
+
+    /**
+     * @ORM\PreRemove()
+     */
+    public function preRemoveUpload()
+    {
+        $this->tempFilename = $this->getUploadRootDir().'/'.$this->id. '.'.$this->extension;
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        if(file_exists($this->tempFilename)){
+            unlink($this->tempFilename);
+        }
+    }
+    public function getUploadDir()
+    {
+        return 'uploads';
+    }
+
+    protected function getUploadRootDir()
+    {
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    public function getWebPath()
+    {
+        return $this->getUploadDir().'/'.$this->getId().'.'.$this->getExtension();
+    }
+}
