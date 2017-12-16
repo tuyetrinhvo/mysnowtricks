c5c82a5c2985d3d4edc4519a4ba83deac24330b9
tuyetrinhvo <you@example.com>
2017-12-16 10:51:41 +0100

    tests for view

--- src/TTV/WebsiteBundle/Controller/WebsiteController.php
+++ src/TTV/WebsiteBundle/Controller/WebsiteController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class WebsiteController extends Controller
 {
-    public function indexAction($page, $nbPerPage)
+    public function indexAction($page)
     {
         if($page < 1)
         {
@@ -23,8 +23,13 @@ class WebsiteController extends Controller
 
     public function viewAction($id)
     {
+        $repository = $this->getDoctrine()->getManager()->getRepository('TTVWebsiteBundle:Trick');
+        $trick = $repository->find($id);
 
-        return $this->render('TTVWebsiteBundle:Website:view.html.twig', ['id' => $id]);
+        if (null === $trick){
+            throw new NotFoundHttpException("La figure d'id ".$id." n'existe pas");
+        }
+        return $this->render('TTVWebsiteBundle:Website:view.html.twig', ['id' => $id, 'trick' => $trick]);
     }
 
     public function addAction(Request $request)
@@ -36,7 +41,7 @@ class WebsiteController extends Controller
             return $this->redirectToRoute('ttv_website_view', ['id' => 3]);
         }
 
-        return $this->render('TTVWebsiteBundle:Trick:add.html.twig');
+        return $this->render('TTVWebsiteBundle:Website:add.html.twig');
     }
 
     public function editAction($id, Request $request)
@@ -48,11 +53,11 @@ class WebsiteController extends Controller
             return $this->redirectToRoute('ttv_website_view', ['id' => 3]);
         }
 
-        return $this->render('TTVWebsiteBundle:Trick:edit.html.twig');
+        return $this->render('TTVWebsiteBundle:Website:edit.html.twig');
     }
 
     public function deleteAction($id)
     {
-        return $this->render('TTVWebsiteBundle:Trick:delete.html.twig');
+        return $this->render('TTVWebsiteBundle:Website:delete.html.twig');
     }
 }
