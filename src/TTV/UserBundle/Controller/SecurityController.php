2d42142ddf773bd8b47e983e773e35cce5c64ff8
tuyetrinhvo <you@example.com>
2017-12-21 00:35:39 +0100

    security configuration

--- src/TTV/UserBundle/Controller/SecurityController.php
+++ src/TTV/UserBundle/Controller/SecurityController.php
@@ -3,11 +3,23 @@
 namespace TTV\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends Controller
 {
-    public function indexAction()
+    public function loginAction(Request $request)
     {
-        return $this->render('login.html.twig');
+        // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')){
+            return $this->redirectToRoute('ttv_website_homepage');
+        }
+
+        // Le service authentication_utils permet de récupérer le nom d'utilisateur et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        return $this->render('TTVUserBundle:Security:login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error'         => $authenticationUtils->getLastAuthenticationError(),
+        ]);
     }
 }
